e0817b9a9120396170cde9424c43ed388bee2b20
mfrank34 <mffranks34@gmail.com>
2024-09-25 23:01:08 +0100

    commit

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-var msg = 'Hello World';
-console.log(msg);
+
+<html>
+<body>
+
+<h2>What Can JavaScript Do?</h2>
+
+<p id="demo">JavaScript can change HTML content.</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+
+</body>
+</html>
