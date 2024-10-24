0fc34566966abd22e2c993f9bbd651c51eda9ded
MFranks34 <mffranks34@gmail.com>
2024-10-24 12:07:49 +0100

    commit

--- Script.js
+++ Script.js
@@ -3,14 +3,32 @@
  // user vailations cansin
 
  // learder board - mike 
- var leaderboard = {
-    // stores information for leaderboard - in memory
- };
 
- var playerData = "PlayerData.json";
+ // goes and fetches the data from player data
+ function fetchJSONData() {
+    fetch("./PlayerData.json")
+        .then((res)=> {
+            if(!res.ok){
+                throw new Error
+                (`HTTP error! Status: ${res.status}`);
+            }
+            return res.json()
+        })
+        .then((data) =>
+            console.log(data))
+        .catch((error) =>
+            console.error("Unable to fetch data:", error))
+}
 
- function update_leaderboard() {
-    
+let leaderboard = fetchJSONData();
+
+ function Update_LeaderBoard() {
+    try{
+        
+    }
+    catch(err) {
+
+    }
  };
 
  // main page interations - mikr
