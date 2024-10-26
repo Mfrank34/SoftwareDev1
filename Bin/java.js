007baf356ce0261802d372d2d3f20a5911279711
mfrank34 <mffranks34@gmail.com>
2024-10-26 11:06:20 +0100

    commit

--- Bin/java.js
+++ Bin/java.js
@@ -7,3 +7,18 @@
 
     document.addEventListener("keydown", event => {handleKey(event)})
 })(); 
+
+function fetchJSONData() {
+    fetch("./PlayerData.json")
+        .then((res)=> {
+            if(!res.ok){
+                throw new Error
+                (`HTTP error! Status: ${res.status}`);
+            }
+            return res.json()
+        })
+        .then((data) =>
+            console.log(data))
+        .catch((error) =>
+            console.error("Unable to fetch data:", error))
+}
--- Script.js
+++ Script.js
@@ -3,34 +3,35 @@
  // user vailations cansin
 
  // learder board - mike 
+const filePath = "PlayerData.json"
+const PlayerData = []
 
-let filePath = "PlayerData.json"
-let PlayerData = {}
-
- function sort(a, b) {
+function sort(a, b) {
     return b.score - a.score; // Sorts in descending order
 }
 
 function FetchPlayerData(PlayerData, filePath) {
     /**
-     * Reads data from a JSON file and appends it to PlayerData
+     * Reads data from a JSON file, appends it to PlayerData, and returns the modified array.
      */
     try {
         const fileContent = fs.readFileSync(filePath, 'utf-8');
         const data = JSON.parse(fileContent);
         
         // Merge and sort data by score in descending order
-        PlayerData.push(...data);
-        PlayerData.sort(sort);
-    // error checking just incase it breaks you know
+        const updatedData = [...PlayerData, ...data];
+        updatedData.sort(sort); // Assuming each player has a 'score' property
+
+        return updatedData;
     } catch (error) {
         if (error instanceof SyntaxError) {
             console.log('No valid PlayerData within file');
         } else {
             console.error(error);
         }
+        return PlayerData; // Return original data if there's an error
     }
-};
+}
 
 function UpdatesPlaterData(PlayerData, filePath) {
     // Updates the user data
@@ -38,7 +39,10 @@ function UpdatesPlaterData(PlayerData, filePath) {
     // Write updated PlayerData to JSON file
     fs.writeFileSync(filePath, JSON.stringify(PlayerData, null, 2));
 }
-
+function Update() {
+    var filePath = "PlayerData.json"
+     return FetchPlayerData(PlayerData, filePath)
+}
 function Update_LeaderBoard() {
     
  };
