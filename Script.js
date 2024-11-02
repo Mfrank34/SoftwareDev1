8d3aaf12c794a2457a7ef9f8f3547fb0134ca780
mfrank34 <mffranks34@gmail.com>
2024-11-02 22:30:23 +0000

    deleted image in bin

--- Script.js
+++ Script.js
@@ -175,7 +175,7 @@ document.addEventListener("DOMContentLoaded", function() {
         // UserWord same as result = true
             if (UserWord == result){
                 document.getElementById("playerCounter").textContent = `Winner!!`
-
+                // disables loop
                 numAttempts = 5
             } else {
                 LivesUpdater(5, numAttempts)
